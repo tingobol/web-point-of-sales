343d45ae6ad71215e340b4e8f39c6e75bcf71c32
iswanputera@gmail.com <iswanputera@gmail.com@d9bae8da-c618-6955-4d1a-9077b6328031>
2013-01-16 10:13:46 +0000

    update default lokasi user

---
+++ trunk/application/views/Laporan/lap_absensi.php
@@ -0,0 +1,9 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+$path='application/views/laporan';
+
+panel_begin('Lap.Absensi');
+panel_multi('Rekap Absensi','block',false);
+
+panel_multi_end();
+panel_end();
+?>
