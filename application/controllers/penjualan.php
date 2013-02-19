6fb15c615aecb6c174811c98bb07521634d0e37f
iswanputera@gmail.com <iswanputera@gmail.com@d9bae8da-c618-6955-4d1a-9077b6328031>
2013-02-19 11:33:45 +0000

    change slip location

--- application/controllers/penjualan.php
+++ application/controllers/penjualan.php
@@ -430,7 +430,7 @@ class Penjualan extends CI_Controller{
 		return $bawah;
 	}
 	function re_print(){
-		system("print ".$this->session->userdata('userid')."_slip.txt");
+		system("print c:\\app\\".$this->session->userdata('userid')."_slip.txt");
 		system("close");
 		$tmpdir=sys_get_temp_dir();
 		
